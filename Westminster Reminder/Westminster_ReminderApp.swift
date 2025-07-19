74468dc063dc62314016fd070bd724a0163924ed
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2025-07-18 22:58:19 -0700

    Initial Commit

---
+++ Westminster Reminder/Westminster_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Westminster_ReminderApp.swift
+//  Westminster Reminder
+//
+//  Created by Ralf Grosse Kunstleve on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Westminster_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Westminster ReminderTests/Westminster_ReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  Westminster_ReminderTests.swift
+//  Westminster ReminderTests
+//
+//  Created by Ralf Grosse Kunstleve on 7/18/25.
+//
+
+import Testing
+@testable import Westminster_Reminder
+
+struct Westminster_ReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
